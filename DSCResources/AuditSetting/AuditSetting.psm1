aac4560d1c3b6bd59df1b1ae125fe1d033ed6a2e
Jason Walker <jwalker@microsoft.com>
2019-06-17 19:11:54 -0400

    Filtered out properties with ClassCreation in name

--- DSCResources/AuditSetting/AuditSetting.psm1
+++ DSCResources/AuditSetting/AuditSetting.psm1
@@ -198,6 +198,7 @@ function Get-CimKeyProperty
     $cimNameSpace, $cimClassName = $CimInstance.CimClass -split ':'
     $cimClassInfo = Get-CimClass -Namespace $cimNameSpace -ClassName $cimClassName
     $propertyNames = $cimClassInfo.CimClassProperties.Where({$PSItem.Flags.HasFlag([Microsoft.Management.Infrastructure.CimFlags]::Key)})
+    $propertyNames = $propertyNames.CimClassProperties.Where{($PSItem.Name -notmatch 'CreationClass')}
     $propertyNames
 }
 
