922f8789afe645cb4557eefee6dd3431287c7ad0
Jason Walker <jwalker@microsoft.com>
2019-06-18 14:23:45 -0400

    added scriptInfo to examples (#2)

--- Examples/Resources/AuditSetting/1-AuditSetting_AuditVolumneNtfsConfig.ps1
+++ Examples/Resources/AuditSetting/1-AuditSetting_AuditVolumneNtfsConfig.ps1
@@ -1,3 +1,42 @@
+<#PSScriptInfo
+.VERSION 1.0.0
+.GUID dfbc4888-efec-4229-a2b6-fe6a25012e61
+.AUTHOR Jason Walker
+.COMPANYNAME
+.COPYRIGHT (c) 2019 Jason Walker. All rights reserved.
+.TAGS DSCConfiguration
+.LICENSEURI https://github.com/jcwalker/AuditSystemDsc/blob/dev/LICENSE
+.PROJECTURI https://github.com/jcwalker/AuditSystemDsc/
+.ICONURI
+.EXTERNALMODULEDEPENDENCIES
+.REQUIREDSCRIPTS
+.EXTERNALSCRIPTDEPENDENCIES
+.RELEASENOTES First version.
+.PRIVATEDATA
+#>
+
+#Requires -module FileContentDsc
+
+<#
+    .DESCRIPTION
+    Set the `Level` entry in the [Logging] section to `Information`
+    in the file `c:\myapp\myapp.ini`.
+#>
+Configuration IniSettingsFile_SetPlainTextEntry_Config
+{
+    Import-DSCResource -ModuleName FileContentDsc
+
+    Node localhost
+    {
+        IniSettingsFile SetLogging
+        {
+            Path    = 'c:\myapp\myapp.ini'
+            Section = 'Logging'
+            Key     = 'Level'
+            Text    = 'Information'
+        }
+    }
+}
 configuration AuditSetting_AuditVolumneNtfsConfig
 {
     Import-DscResource -ModuleName AuditSystemDsc
--- Examples/Resources/AuditSetting/2-AuditSetting_AuditUsersPasswordNotRequiredConfig.ps1
+++ Examples/Resources/AuditSetting/2-AuditSetting_AuditUsersPasswordNotRequiredConfig.ps1
@@ -1,3 +1,19 @@
+<#PSScriptInfo
+.VERSION 1.0.0
+.GUID 0ea958da-bb97-41c6-a76f-9125930e72b3
+.AUTHOR Jason Walker
+.COMPANYNAME
+.COPYRIGHT (c) 2019 Jason Walker. All rights reserved.
+.TAGS DSCConfiguration
+.LICENSEURI https://github.com/jcwalker/AuditSystemDsc/blob/dev/LICENSE
+.PROJECTURI https://github.com/jcwalker/AuditSystemDsc/
+.ICONURI
+.EXTERNALMODULEDEPENDENCIES
+.REQUIREDSCRIPTS
+.EXTERNALSCRIPTDEPENDENCIES
+.RELEASENOTES First version.
+.PRIVATEDATA
+#>
 configuration AuditSetting_AuditUsersPasswordNotRequiredConfig
 {
     Import-DscResource -ModuleName AuditSystemDsc
